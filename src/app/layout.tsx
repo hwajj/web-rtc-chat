216f8a42535920085fcffe059c21595764bf2651
hwajj <azamwara@gmail.com>
2024-09-12 12:15:05 +0900

    [feat] header 및 footer 추가
    - logo 추가
    - layout 스타일 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} px-10 pt-10 pb-2 antialiased w-screen h-screen m-0 p-0 flex flex-col absolute`}
       >
+        <header className='p-2 flex items-baseline'>
+          <Image
+            className="dark:invert"
+            src="/logo2.png"
+            alt="Next.js logo"
+            width={312.5}
+            height={72.5}
+            priority
+          />
+        </header>
+
+
         {children}
+        <footer className=' mt-auto'>
+          hjeong
+        </footer>
       </body>
     </html>
   );
